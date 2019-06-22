9e8fad0a987f13c131c2bb477fe293e8f3fc4d5e
analyst <analyst@elk.windomain.local>
2019-06-22 10:20:52 -0500

    add_hosts to playbook

--- get-win-ip.sh
+++ get-win-ip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ip route get 1 | awk '{print $NF;exit}' | sed 's/\.[0-9]*$/.*/' | sudo nmap -sP -iL - -oG - | grep -v Nmap | cut -d " " -f2 | sudo nmap -p 445 --script smb-os-discovery -iL - | grep dc.windomain.local -B 16 | grep "scan report for" | cut -d " " -f5
+ip route get 1 | awk '{print $NF;exit}' | sed 's/\.[0-9]*$/.*/' | sudo nmap -sP -iL - -oG - | grep -v Nmap | cut -d " " -f2 | sudo nmap -p 445 --script smb-os-discovery -iL - | grep dc.windomain.local -B 16 | grep "scan report for" | cut -d " " -f5 | tr -d '\n'
