38ca3e867f6169159e262a38aac2c94bdbcee954
analyst <analyst@elk.windomain.local>
2019-06-23 01:18:54 -0500

    Fixed shell scripts

--- config-osquery.sh
+++ config-osquery.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "[WINDOWS-DC] Looking for Windows DC on the local network"
-while [[ $(ip route get 1 | awk '{print $NF;exit}' | sed 's/\.[0-9]*$/.*/' | sudo nmap -sP -iL - -oG - | grep -v Nmap | cut -d " " -f2 | sudo nmap -p 445 --script smb-os-discovery -iL - | grep dc.windomain.local -B 16 | grep "scan report for" | cut -d " " -f5 | tr -d '\n' | wc -l) < 1 ]] ; do
+while [[ $(./get-win-ip.sh | wc -w) < 1 ]] ; do
   echo "[WINDOWS-DC] Cannot locate Windows DC"
   sleep 10
 done
