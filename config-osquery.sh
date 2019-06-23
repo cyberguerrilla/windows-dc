cdc8c62e2e2d5f7ce17ce5dfb5a6b8192ab8fd06
analyst <analyst@elk.windomain.local>
2019-06-23 18:25:48 -0500

    Fix WinDC playbooks

--- config-osquery.sh
+++ config-osquery.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-ansible-playbook ./check-prereqs.yml
-
 echo "[WINDOWS-DC] Looking for Windows DC on the local network"
 while [[ $(./get-win-ip.sh | wc -w) < 1 ]] ; do
   echo "[WINDOWS-DC] Cannot locate Windows DC"
