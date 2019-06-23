6b95f3fe2dad318613d9d32a3267d0c17b2d47fd
analyst <analyst@elk.windomain.local>
2019-06-23 16:00:06 -0500

    Fixed shell scripts again

--- config-osquery.sh
+++ config-osquery.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ansible-playbook ./check-prereqs.yml
+
 echo "[WINDOWS-DC] Looking for Windows DC on the local network"
 while [[ $(./get-win-ip.sh | wc -w) < 1 ]] ; do
   echo "[WINDOWS-DC] Cannot locate Windows DC"
