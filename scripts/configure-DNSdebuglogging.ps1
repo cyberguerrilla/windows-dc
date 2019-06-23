ba44305da041c86b1f811f83f1479f63ef0ec9a5
analyst <analyst@elk.windomain.local>
2019-06-23 00:12:18 -0500

    Reorganize playbooks

--- scripts/configure-DNSdebuglogging.ps1
+++
@@ -1,7 +0,0 @@
-New-Item -ItemType Directory -Force -Path "C:\DNSLogs"
-Set-DnsServerDiagnostics -FullPackets $True -SendPackets $True -TcpPackets $True -Answers $True `
--Updates $True -UdpPackets $True -ReceivePackets $True -Queries $True -QuestionTransactions $True `
--EnableLogFileRollover $True -EnableLoggingToFile $True -LogFilePath "C:\DNSLogs\dns.log" `
--MaxMBFileSize 500000000
-Start-Sleep -Seconds 5
-Restart-Service DNS
