14304fea8ee5da7668b7c21bc4be05d4a3948f77
analyst <analyst@elk.windomain.local>
2019-06-25 01:01:09 -0500

    Change config osquery to include flags copy

--- config-osquery.sh
+++ config-osquery.sh
@@ -13,7 +13,7 @@ while [[ $(docker ps -q -f name=fleet -f status=running | wc -l) < 1 ]] ; do
 done
 
 echo "[OSQUERY] Checking fleetctl status"
-while [[ $(sudo docker exec -i fleet /bin/sh -c 'fleetctl login --email guerillaBT@bsides.lab --password bsid3s!' | grep -c "Fleet login successful" 2> /dev/null) < 1 ]] ; do
+while [[ $(sudo docker exec -i fleet /bin/sh -c 'fleetctl login --email guerillaBT@bsides.lab --password bsid3s! 2> /dev/null' | grep -c "Fleet login successful") < 1 ]] ; do
   echo "[OSQUERY] Fleetctl is not up yet"
   sleep 5
 done
